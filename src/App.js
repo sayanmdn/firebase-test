fd4629e13e8a3a14e55ef1076c425dae29e116ce
Sayantan Mishra <sayanmdn@gmail.com>
2020-10-11 20:31:41 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css'
+import * as firebase from 'firebase';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: 32
+    }
+    this.handleEvent = this.handleEvent.bind(this);
+  }
+  handleEvent() {
+    // console.log(this.props);
+    this.setState({
+      data: this.state.data + 1
+    })
+  }
+  componentDidMount(){
+    const rootRef = firebase.database().ref().child()
+    const speedRef = rootRef.child('speed')
+    speedRef.on('value',snap =>{
+      this.setState({
+        speed:snap.val()
+      })
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.data}</h1>
+        <button onClick={this.handleEvent}>Chick here</button>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default App;  
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyAVEQNVvdEigNTnu01mDJmr3ThVBBPpsH8",
+  authDomain: "fir-test-3a27d.firebaseapp.com",
+  databaseURL: "https://fir-test-3a27d.firebaseio.com",
+  projectId: "fir-test-3a27d",
+  storageBucket: "fir-test-3a27d.appspot.com",
+  messagingSenderId: "693630689099",
+  appId: "1:693630689099:web:a90795c2c2b8761ebf52ce",
+  measurementId: "G-9WMXB8CJCK"
+};
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
   <React.StrictMode>
