f7ab91bce877624f5e2dfbb420f7862e61070237
Sayantan Mishra <sayanmdn@gmail.com>
2020-10-14 11:43:16 +0530

    class active

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css'
 import * as firebase from 'firebase';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,17 +11,16 @@ class App extends Component {
     this.handleEvent = this.handleEvent.bind(this);
   }
   handleEvent() {
-    // console.log(this.props);
     this.setState({
       data: this.state.data + 1
     })
   }
-  componentDidMount(){
-    const rootRef = firebase.database().ref().child()
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('react')
     const speedRef = rootRef.child('speed')
-    speedRef.on('value',snap =>{
+    speedRef.on('value', snap => {
       this.setState({
-        speed:snap.val()
+        data: snap.val()
       })
     })
   }
